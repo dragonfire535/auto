4268de83024e4a31e62f92102c04926d2bc3c40d
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-10-29 21:09:04 +0000

    commando

--- Auto.js
+++ Auto.js
@@ -1,18 +1,33 @@
-const { AUTO_TOKEN } = process.env;
-const { Client } = require('discord.js');
-const client = new Client({
+const { AUTO_TOKEN, AUTO_COMMAND_PREFIX, OWNERS, INVITE } = process.env;
+const path = require('path');
+const { CommandoClient } = require('discord.js-commando');
+const client = new CommandoClient({
+	commandPrefix: AUTO_COMMAND_PREFIX,
+	owner: OWNERS.split(','),
+	invite: INVITE,
 	disableEveryone: true,
+	unknownCommandResponse: false,
 	disabledEvents: ['TYPING_START']
 });
-const { linter } = require('eslint');
-const { stripIndents } = require('common-tags');
-const eslintConfig = require('./assets/json/eslintConfig');
-const goodMessages = require('./assets/json/goodMessages');
-const badMessages = require('./assets/json/badMessages');
+
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([
+		['util', 'Utility'],
+		['lint', 'Lint'],
+		['other', 'Other']
+	])
+	.registerDefaultCommands({
+		help: false,
+		ping: false,
+		prefix: false,
+		commandState: false
+	})
+	.registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.on('ready', () => {
 	console.log(`[READY] Logged in as ${client.user.tag}! (${client.user.id})`);
-	client.user.setActivity(`@${client.user.tag} to scan!`);
+	client.user.setActivity(`@${client.user.tag} lint to scan!`);
 });
 
 client.on('disconnect', event => {
@@ -23,32 +38,6 @@ client.on('disconnect', event => {
 client.on('error', err => console.error('[ERROR]', err));
 
 client.on('warn', err => console.warn('[WARNING]', err));
-
-const validMessage = /```(.|\s)+```/gi;
-client.on('message', async msg => {
-	if (!validMessage.test(msg.content)) {
-		if (msg.mentions.has(client.user.id)) await msg.reply('Invalid message!');
-		return;
-	}
-	const test = msg.content.match(validMessage)[0].replace(/```(js|javascript)?|```/gi, '').trim();
-	const errors = linter.verify(test, eslintConfig);
-	if (!errors.length) {
-		await msg.react('✅');
-		if (msg.mentions.has(client.user.id)) {
-			const resultMsg = goodMessages[Math.floor(Math.random() * goodMessages.length)];
-			await msg.reply(resultMsg);
-		}
-	} else {
-		await msg.react('❌');
-		if (msg.mentions.has(client.user.id)) {
-			const resultMsg = badMessages[Math.floor(Math.random() * badMessages.length)];
-			await msg.reply(stripIndents`
-				${resultMsg}
-				${errors.map(err => `\`${err.line}:${err.column} ${err.message}\``).join('\n')}
-			`);
-		}
-	}
-});
 
 client.login(AUTO_TOKEN);
 
---
+++ commands/lint/lint-default.js
@@ -0,0 +1,45 @@
+const { Command } = require('discord.js-commando');
+const { linter } = require('eslint');
+const { stripIndents } = require('common-tags');
+const eslintConfig = require('../../assets/json/eslint-default');
+const goodMessages = require('../../assets/json/good-messages');
+const badMessages = require('../../assets/json/bad-messages');
+const codeblock = /```(.|\s)+```/gi;
+
+module.exports = class LintDefaultCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'lint-default',
+			aliases: ['lint-recommended', 'lint'],
+			group: 'lint',
+			memberName: 'lint-default',
+			description: 'Lints code with the recommended rules.',
+			patterns: [codeblock],
+			clientPermissions: ['ADD_REACTIONS', 'READ_MESSAGE_HISTORY'],
+			args: [
+				{
+					key: 'code',
+					prompt: 'What code do you want to lint?',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	run(msg, { code }, pattern) {
+		if (pattern || code.test(codeblock)) code = code.match(codeblock)[0].replace(/```(js|javascript)?|```/gi, '').trim();
+		const errors = linter.verify(code, eslintConfig);
+		if (!errors.length) {
+			await msg.react('✅');
+			if (pattern) return null;
+			return msg.reply(goodMessages[Math.floor(Math.random() * goodMessages.length)]);
+		} else {
+			await msg.react('❌');
+			if (pattern) return null;
+			return msg.reply(stripIndents`
+				${badMessages[Math.floor(Math.random() * badMessages.length)]}
+				${errors.map(err => `\`[${err.line}:${err.column}] ${err.message}\``).join('\n')}
+			`);
+		}
+	}
+};
