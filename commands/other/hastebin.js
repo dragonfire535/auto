f05fce81176b56d0fa9133f851dd935dd2995945
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2017-11-01 11:24:15 +0000

    hastebin command

---
+++ commands/other/hastebin.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord.js-commando');
+const snekfetch = require('snekfetch');
+
+module.exports = class HastebinCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'hastebin',
+			aliases: ['pastebin'],
+			group: 'other',
+			memberName: 'hastebin',
+			description: 'Posts code to hastebin.',
+			args: [
+				{
+					key: 'code',
+					prompt: 'What code do you want to post to hastebin?',
+					type: 'code'
+				}
+			]
+		});
+	}
+
+	async run(msg, { code }) {
+		try {
+			const { body } = await snekfetch
+				.post('https://hastebin.com/documents')
+				.send(code.code);
+			return msg.reply(`https://hastebin.com/${body.key}.${code.lang || 'js'}`);
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
