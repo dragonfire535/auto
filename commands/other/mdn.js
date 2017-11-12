0b55518f31c4f544effe6ba2ef01dc1be1541f4f
Will Nelson <nelson.will@live.com>
2017-11-12 10:51:41 -0800

    make mdn great again (#5)
    * make mdn great again
    
    * Update mdn.js
    
    * fix embedded links
    
    * Update mdn.js

--- commands/other/mdn.js
+++ commands/other/mdn.js
@@ -1,6 +1,8 @@
 const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
 const snekfetch = require('snekfetch');
+const toMarkdown = require('to-markdown');
+const root = 'https://developer.mozilla.org';
 
 module.exports = class MDNCommand extends Command {
 	constructor(client) {
@@ -25,20 +27,20 @@ module.exports = class MDNCommand extends Command {
 	async run(msg, { query }) {
 		try {
 			const { body } = await snekfetch
-				.get('https://developer.mozilla.org/en-US/search.json')
-				.query({
-					q: query,
-					locale: 'en-US',
-					highlight: false
-				});
-			if (!body.documents.length) return msg.say('Could not find any results.');
-			const data = body.documents[0];
+				.get('https://mdn.topkek.pw/search')
+				.query({ q: query });
+			if (!body.URL || !body.Title || !body.Summary) return msg.say('Could not find any results.');
 			const embed = new MessageEmbed()
 				.setColor(0x066FAD)
 				.setAuthor('MDN', 'https://i.imgur.com/DFGXabG.png')
-				.setURL(data.url)
-				.setTitle(data.title)
-				.setDescription(data.excerpt);
+				.setURL(`${root}${body.URL}`)
+				.setTitle(body.Title)
+				.setDescription(toMarkdown(body.Summary, {
+					converters: [{
+						filter: 'a',
+						replacement: (text, node) => `[${text}](${root}${node.href})`
+					}]
+				}));
 			return msg.embed(embed);
 		} catch (err) {
 			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
