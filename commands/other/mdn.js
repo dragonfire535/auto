2c6a3406bbd351319721a23c49b51a992099ba9f
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-01-13 21:01:30 +0000

    Deps

--- commands/other/mdn.js
+++ commands/other/mdn.js
@@ -1,7 +1,7 @@
 const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
 const snekfetch = require('snekfetch');
-const toMarkdown = require('to-markdown');
+const Turndown = require('turndown');
 const root = 'https://developer.mozilla.org';
 
 module.exports = class MDNCommand extends Command {
@@ -30,17 +30,17 @@ module.exports = class MDNCommand extends Command {
 				.get('https://mdn.topkek.pw/search')
 				.query({ q: query });
 			if (!body.URL || !body.Title || !body.Summary) return msg.say('Could not find any results.');
+			const turndown = new Turndown();
+			turndown.addRule('hyperlink', {
+				filter: 'a',
+				replacement: (text, node) => `[${text}](${root}${node.href})`
+			});
 			const embed = new MessageEmbed()
 				.setColor(0x066FAD)
 				.setAuthor('MDN', 'https://i.imgur.com/DFGXabG.png')
 				.setURL(`${root}${body.URL}`)
 				.setTitle(body.Title)
-				.setDescription(toMarkdown(body.Summary, {
-					converters: [{
-						filter: 'a',
-						replacement: (text, node) => `[${text}](${root}${node.href})`
-					}]
-				}));
+				.setDescription(turndown.turndown(body.Summary));
 			return msg.embed(embed);
 		} catch (err) {
 			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
