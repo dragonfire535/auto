1578bd7f0eef39871b083a643c6e2b2e81996aeb
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-03-12 18:42:37 +0000

    base64 decode

--- commands/text-edit/base64.js
+++ commands/text-edit/base64.js
@@ -1,4 +1,6 @@
 const { Command } = require('discord.js-commando');
+const { list, base64 } = require('../../util/Util');
+const modes = ['encode', 'decode'];
 
 module.exports = class Base64Command extends Command {
 	constructor(client) {
@@ -8,13 +10,24 @@ module.exports = class Base64Command extends Command {
 			group: 'text-edit',
 			memberName: 'base64',
 			description: 'Converts text to Base64.',
+			details: `**Modes**: ${modes.join(', ')}`,
 			args: [
+				{
+					key: 'mode',
+					prompt: `Would you like to ${list(modes, 'or')}?`,
+					type: 'string',
+					validate: mode => {
+						if (modes.includes(mode.toLowerCase())) return true;
+						return `Invalid mode, please enter either ${list(modes, 'or')}.`;
+					},
+					parse: mode => mode.toLowerCase()
+				},
 				{
 					key: 'text',
 					prompt: 'What text would you like to convert to Base64?',
 					type: 'string',
 					validate: text => {
-						if (Buffer.from(text).toString('base64').length < 2000) return true;
+						if (base64(text).length < 2000) return true;
 						return 'Invalid text, your text is too long.';
 					}
 				}
@@ -22,7 +35,7 @@ module.exports = class Base64Command extends Command {
 		});
 	}
 
-	run(msg, { text }) {
-		return msg.say(Buffer.from(text).toString('base64'));
+	run(msg, { mode, text }) {
+		return msg.say(base64(text, mode));
 	}
 };
--- util/Util.js
+++ util/Util.js
@@ -6,6 +6,11 @@ class Util {
 		return `${hrs.padStart(2, '0')}:${min.padStart(2, '0')}:${sec.padStart(2, '0')}`;
 	}
 
+	static list(arr, conj = 'and') {
+		const len = arr.length;
+		return `${arr.slice(0, -1).join(', ')}${len > 1 ? `${len > 2 ? ',' : ''} ${conj} ` : ''}${arr.slice(-1)}`;
+	}
+
 	static shorten(text, maxLen = 2000) {
 		return text.length > maxLen ? `${text.substr(0, maxLen - 3)}...` : text;
 	}
@@ -17,6 +22,12 @@ class Util {
 			arr.push(`${len} more...`);
 		}
 		return arr;
+	}
+
+	static base64(text, mode = 'encode') {
+		if (mode === 'encode') return Buffer.from(text).toString('base64');
+		if (mode === 'decode') return Buffer.from(text, 'base64').toString('utf8');
+		throw new TypeError(`${mode} is not a supported base64 mode.`);
 	}
 }
 
