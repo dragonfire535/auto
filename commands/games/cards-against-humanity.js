fb19fcdcb998acde4bd5e7ea5a9a50317382c794
Dragon Fire <danielbodendahl@gmail.com>
2018-08-10 18:16:04 -0400

    Remove DM check from CAH

--- commands/games/cards-against-humanity.js
+++ commands/games/cards-against-humanity.js
@@ -34,7 +34,7 @@ module.exports = class CardsAgainstHumanityCommand extends Command {
 		let joinLeaveCollector = null;
 		try {
 			await msg.util.sendNew('You will need at least 2 more players, at maximum 10. To join, type `join game`.');
-			const awaitedPlayers = await awaitPlayers(msg, 10, 3, { dmCheck: true });
+			const awaitedPlayers = await awaitPlayers(msg, 10, 3);
 			if (!awaitedPlayers) {
 				this.playing.delete(msg.channel.id);
 				return msg.util.sendNew('Game could not be started...');
