bbfda473c102e3d7695954fffacb72aae83c47ef
Dragon Fire <danielbodendahl@gmail.com>
2018-08-14 19:52:58 -0400

    Remove games before archive

--- util/Util.js
+++ util/Util.js
@@ -1,7 +1,3 @@
-const yes = ['yes', 'y', 'ye', 'yeah', 'yup', 'yea'];
-const no = ['no', 'n', 'nah', 'nope'];
-const { SUCCESS_EMOJI_ID } = process.env;
-
 class Util {
 	static shuffle(array) {
 		const arr = array.slice(0);
@@ -51,48 +47,6 @@ class Util {
 		if (mode === 'encode') return Buffer.from(text).toString('base64');
 		if (mode === 'decode') return Buffer.from(text, 'base64').toString('utf8') || null;
 		throw new TypeError(`${mode} is not a supported base64 mode.`);
-	}
-
-	static async awaitPlayers(msg, max, min, { text = 'join game', time = 30000, dmCheck = false } = {}) {
-		const joined = [];
-		joined.push(msg.author.id);
-		const filter = res => {
-			if (res.author.bot) return false;
-			if (joined.includes(res.author.id)) return false;
-			if (res.content.toLowerCase() !== text.toLowerCase()) return false;
-			joined.push(res.author.id);
-			res.react(SUCCESS_EMOJI_ID || '✅').catch(() => null);
-			return true;
-		};
-		const verify = await msg.channel.awaitMessages(filter, { max, time });
-		verify.set(msg.id, msg);
-		if (dmCheck) {
-			for (const message of verify.values()) {
-				try {
-					await message.author.send('Hi! Just testing that DMs work, pay this no mind.');
-				} catch (err) {
-					verify.delete(message.id);
-				}
-			}
-		}
-		if (verify.size < min) return false;
-		return verify.map(message => message.author);
-	}
-
-	static async verify(channel, user, time = 30000) {
-		const filter = res => {
-			const value = res.content.toLowerCase();
-			return res.author.id === user.id && (yes.includes(value) || no.includes(value));
-		};
-		const verify = await channel.awaitMessages(filter, {
-			max: 1,
-			time
-		});
-		if (!verify.size) return 0;
-		const choice = verify.first().content.toLowerCase();
-		if (yes.includes(choice)) return true;
-		if (no.includes(choice)) return false;
-		return false;
 	}
 }
 
