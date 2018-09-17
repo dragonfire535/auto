16172ea49916e426541c62e870ac891ca668732e
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-09-17 23:49:04 +0000

    Add Dependencies and Source Code to Info

--- commands/util/info.js
+++ commands/util/info.js
@@ -1,8 +1,9 @@
 const { Command } = require('discord-akairo');
 const { MessageEmbed } = require('discord.js');
-const { version } = require('../../package');
+const { version, dependencies } = require('../../package');
 const { duration } = require('../../util/Util');
-const { INVITE } = process.env;
+const { INVITE, AUTO_GITHUB_REPO_USERNAME, AUTO_GITHUB_REPO_NAME } = process.env;
+const source = AUTO_GITHUB_REPO_NAME && AUTO_GITHUB_REPO_USERNAME;
 
 module.exports = class InfoCommand extends Command {
 	constructor() {
@@ -22,12 +23,21 @@ module.exports = class InfoCommand extends Command {
 			.addField('❯ Shards', this.client.options.shardCount, true)
 			.addField('❯ Commands', this.handler.modules.size, true)
 			.addField('❯ Home Server', INVITE ? `[Here](${INVITE})` : 'None', true)
+			.addField('❯ Source Code',
+				source ? `[Here](https://github.com/${AUTO_GITHUB_REPO_USERNAME}/${AUTO_GITHUB_REPO_NAME})` : 'N/A', true)
 			.addField('❯ Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
 			.addField('❯ Uptime', duration(this.client.uptime), true)
 			.addField('❯ Version', `v${version}`, true)
 			.addField('❯ Node Version', process.version, true)
-			.addField('❯ Library',
-				'[discord.js](https://discord.js.org) + [discord-akairo](https://github.com/1Computer1/discord-akairo)', true);
+			.addField('❯ Dependencies', Object.entries(dependencies).map(dep => this.parseDependency(dep)).join(', '));
 		return msg.util.send({ embed });
+	}
+
+	parseDependency(dep) {
+		if (dep[1].startsWith('github:')) {
+			const repo = dep[1].replace('github:', '').split('/');
+			return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#.+/, '')})`;
+		}
+		return `[${dep[0]}](https://www.npmjs.com/package/${dep[0]})`;
 	}
 };
