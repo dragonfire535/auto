1319be321e59eae5ae273fa620c19089db849eff
Daniel Odendahl Jr <danielbodendahl@gmail.com>
2018-09-28 15:08:55 +0000

    Update info command

--- commands/util/info.js
+++ commands/util/info.js
@@ -1,7 +1,8 @@
 const { Command } = require('discord-akairo');
 const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
 const { version, dependencies } = require('../../package');
-const { duration } = require('../../util/Util');
 const { INVITE, AUTO_GITHUB_REPO_USERNAME, AUTO_GITHUB_REPO_NAME } = process.env;
 const source = AUTO_GITHUB_REPO_NAME && AUTO_GITHUB_REPO_USERNAME;
 
@@ -18,7 +19,7 @@ module.exports = class InfoCommand extends Command {
 	exec(msg) {
 		const embed = new MessageEmbed()
 			.setColor(0x00AE86)
-			.setFooter('©2017-2018 dragonfire535#8081')
+			.setFooter('©2018 dragonfire535#8081')
 			.addField('❯ Servers', this.client.guilds.size, true)
 			.addField('❯ Shards', this.client.options.shardCount, true)
 			.addField('❯ Commands', this.handler.modules.size, true)
@@ -26,18 +27,20 @@ module.exports = class InfoCommand extends Command {
 			.addField('❯ Source Code',
 				source ? `[Here](https://github.com/${AUTO_GITHUB_REPO_USERNAME}/${AUTO_GITHUB_REPO_NAME})` : 'N/A', true)
 			.addField('❯ Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
-			.addField('❯ Uptime', duration(this.client.uptime), true)
+			.addField('❯ Uptime', moment.duration(this.client.uptime).format('hh:mm:ss', { trim: false }), true)
 			.addField('❯ Version', `v${version}`, true)
 			.addField('❯ Node Version', process.version, true)
-			.addField('❯ Dependencies', Object.entries(dependencies).map(dep => this.parseDependency(dep)).join(', '));
+			.addField('❯ Dependencies', this.parseDependencies());
 		return msg.util.send({ embed });
 	}
 
-	parseDependency(dep) {
-		if (dep[1].startsWith('github:')) {
-			const repo = dep[1].replace('github:', '').split('/');
-			return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#.+/, '')})`;
-		}
-		return `[${dep[0]}](https://www.npmjs.com/package/${dep[0]})`;
+	parseDependencies() {
+		return Object.entries(dependencies).map(dep => {
+			if (dep[1].startsWith('github:')) {
+				const repo = dep[1].replace('github:', '').split('/');
+				return `[${dep[0]}](https://github.com/${repo[0]}/${repo[1].replace(/#.+/, '')})`;
+			}
+			return `[${dep[0]}](https://npmjs.com/${dep[0]})`;
+		}).join(', ');
 	}
 };
--- util/Util.js
+++ util/Util.js
@@ -3,13 +3,6 @@ class Util {
 		return text.length > maxLen ? `${text.substr(0, maxLen - 3)}...` : text;
 	}
 
-	static duration(ms) {
-		const sec = Math.floor((ms / 1000) % 60).toString();
-		const min = Math.floor((ms / (1000 * 60)) % 60).toString();
-		const hrs = Math.floor(ms / (1000 * 60 * 60)).toString();
-		return `${hrs.padStart(2, '0')}:${min.padStart(2, '0')}:${sec.padStart(2, '0')}`;
-	}
-
 	static trimArray(arr, maxLen = 10) {
 		if (arr.length > maxLen) {
 			const len = arr.length - maxLen;
